89f0cbd5bda7992ef474aaae8dfd3705d8969a5d
Sachin Iyer <sachinjiyer@gmail.com>
2023-07-15 03:41:42 -0700

    first exercises finished

--- exercises/variables/variables4/main.go
+++ exercises/variables/variables4/main.go
@@ -1,17 +1,16 @@
 // variables4
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	x := "TEN" // Don't change this line
+	x := "TEN"
 	fmt.Printf("x has the value %s", x)
 
 	if true {
-		x = 1
+		var x = 1
 		fmt.Println(x + 1)
 	}
 
